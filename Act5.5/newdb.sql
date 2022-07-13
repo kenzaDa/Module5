117d490afcbe4b7290adf6ee3a6e55b9b3e2c97c
Kenza Daghrir <kdaghrir@dom.tti>
2022-07-13 15:08:40 +0100

    first commit

---
+++ Act5.5/newdb.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 11 juil. 2022 à 13:32
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `newdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20220707105049', '2022-07-07 12:51:16', 499);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`) VALUES
+(10, 'lina@gmail.com', '[\"ROLE_USER\"]', '$argon2id$v=19$m=65536,t=4,p=1$eU80MGZmN0J0WTdBL0w5OQ$KMQG2NuwVQow1fKGAIX9UFTxJLzngJj0qTvwO3S8Ilk'),
+(11, 'senda@gmail.com', '[\"ROLE_USER\"]', '$argon2id$v=19$m=65536,t=4,p=1$NlRqTWFvUFplbndENEJtZQ$b9a9GQT0zaMMH5Cxnq/ZxymB9uF2shnw8HHmY6xffzQ'),
+(12, 'amani@gmail.com', '[\"ROLE_USER\"]', '$argon2id$v=19$m=65536,t=4,p=1$SkxEQXBPTllTNFl3N29nZQ$77A0TCdsNe/I2K58Nrh8pS238NSAWRT1e63Evxx3eb0'),
+(13, 'olfa@gmail.com', '[\"ROLE_USER\"]', '$argon2id$v=19$m=65536,t=4,p=1$U3l4SXpFTEZkZzl3MGFxag$OhlMK/GwKZYzUJhwFUxP+bgwM9Tn7m69hX4+AvEkiPw'),
+(15, 'kenzaadmin@gmail.com', '[\"ROLE_ADMIN\"]', '$argon2id$v=19$m=65536,t=4,p=1$M25FaVdzV1RicEFIRzNnNA$kl/pEI4+4lQcC1WxLWNrtppV+UqCuloYe309Q6+7Qcw');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Act5.5/src/Controller/ViewsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+
+use Symfony\Component\HttpFoundation\Request;
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\DataTableFactory;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ViewsController extends AbstractController
+{
+    /**
+     * @Route("profile/views", name="_profiler_home")
+     */
+    public function index(): Response
+   
+        {
+            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+            return $this->render('views/index.html.twig', [
+            'controller_name' => 'ViewsController',
+        ]);
+    }
+
+
+
+     /**
+     * @Route("/admin/views1", name="_admin_home")
+     */
+    public function showAction(Request $request, DataTableFactory $dataTableFactory):Response
+    { 
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+      
+        
+        $table = $dataTableFactory->create()
+            ->add('id', TextColumn::class)
+            ->add('email', TextColumn::class)
+            ->createAdapter(ORMAdapter::class, [
+                'entity' => User::class,
+            ])
+            ->handleRequest($request);
+        if ($table->isCallback()) {
+            return $table->getResponse();
+        }
+      
+        return $this->render('views/view1.html.twig', ['datatable' => $table]);
+
+            // public function show(): Response
+    // {  $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    //     return $this->render('views/view1.html.twig');
+    // }
+    }
+
+
+
+
+     /**
+     * @Route("views2", name="_anonymos_page")
+     */
+    public function show2(): Response
+    {
+        return $this->render('views/view2.html.twig');
+    }
+}
