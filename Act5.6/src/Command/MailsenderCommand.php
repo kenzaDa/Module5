93016d0752515bb0fca288cd0535a5695ca38034
Kenza Daghrir <kdaghrir@dom.tti>
2022-07-14 15:12:04 +0100

    update

--- Act5.6/src/Command/MailsenderCommand.php
+++ Act5.6/src/Command/MailsenderCommand.php
@@ -6,35 +6,48 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use App\Repository\UserRepository;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class MailsenderCommand extends Command
 {
    
-    
+        private $MailerInterface;
+
         protected static $defaultName = 'app:Mailsender';
-        private $userRepository;
-        public function __construct(UserRepository $userRepository)
+       
+       
+        public function __construct( MailerInterface $MailerInterface)
         {
+            
+            $this->MailerInterface = $MailerInterface;
             parent::__construct(null);
-            $this->userRepository = $userRepository;
         }
+       
+
         protected function configure()
         {
             $this
                 ->setDescription('Send daily emails to users')
             ;
         }
-        protected function execute(InputInterface $input, OutputInterface $output)
-        {
-            $io = new SymfonyStyle($input, $output);
-            $users = $this->userRepository
-                ->findAll();
-            $io->progressStart(count($users));
-            foreach ($users as $user) {
-                $io->progressAdvance();
-            }
-            $io->progressFinish();
-            $io->success('Weekly reports were sent to authors!');
+        protected function execute(InputInterface $input, OutputInterface $output )
+        {   
+            
+            $email = (new Email())
+            ->from('kenza.daghrir@talan.com')
+            ->to('kenza.daghrir@talan.com')
+          
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>Bonjour </p>');
+
+            $this->MailerInterface->send($email);
+         
+            
+         
+           
+           
         }
     }
